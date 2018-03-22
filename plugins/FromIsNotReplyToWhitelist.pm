5d5dd2cc208ef68d01d02995c4cdc4a367bc1aac
eXtremeSHOK <extremeshok@users.noreply.github.com>
2018-03-22 08:33:40 +0200

    Update FromIsNotReplyToWhitelist.pm
    Allow whitelisting of your contact form, which replaces the replyto with the customers address

--- plugins/FromIsNotReplyToWhitelist.pm
+++ plugins/FromIsNotReplyToWhitelist.pm
@@ -5,7 +5,7 @@
 ##################
 # Spam often uses a different From: and Reply-To:
 # Whilst most legitimate email does not
-# Sometimes the From: and Reply-To: are different, 
+# Sometimes the From: and Reply-To: are different,
 # but the domains will be the same.
 # If the domains are different the email is sapm.
 ##################
@@ -16,12 +16,12 @@ package FromIsNotReplyToWhitelist;
   our @ISA = qw(Mail::SpamAssassin::Plugin);
   sub new {
     my ($class, $mailsa) = @_;
-    
+
     # the usual perlobj boilerplate to create a subclass object
     $class = ref($class) || $class;
     my $self = $class->SUPER::new($mailsa);
     bless ($self, $class);
-   
+
     # then register an eval rule, if desired...
     $self->register_eval_rule ("check_for_from_is_not_reply_to_whitelist");
     # and return the new plugin object
@@ -31,52 +31,52 @@ package FromIsNotReplyToWhitelist;
 sub check_for_from_is_not_reply_to_whitelist {
 	my ($self, $msg) = @_;
 	my $check_from = lc($msg->get( 'From:addr' ));
+	my $check_from_address = lc($msg->get( 'From:addr' ));
+	my $check_from_full_address = lc($msg->get( 'From:addr' ));
 	$check_from =~ s/.*@//;
 	my $check_replyTo = lc($msg->get( 'Reply-To:addr' ));
 	$check_replyTo =~ s/.*@//;
 
 	Mail::SpamAssassin::Plugin::dbg( "FromIsNotReplyToWhitelist: Comparing '$check_from'/'$check_replyTo" );
 
+	##################
+	# Allow whitelisting of your contact form, which replaces the replyto with the customers address
+	# Uncomment the following and replace noreply@domain.tld with a full email address
+	##################
+	#if ( $check_from_full_address eq 'noreply@domain.tld' ) {
+	#		return 1
+	#}
+
+	##################
+	# Whitelisting of common domains which flag
+	##################
 	if ( $check_from eq 'life.thinkingahead.co.za' && $check_replyTo eq 'sanlam.co.za' ) {
 			return 1
-	}
-	if ( $check_from eq 'accounting.sageone.co.za' && $check_replyTo eq 'hitecsecurity.co.za' ) {
+	} elsif ( $check_from eq 'accounting.sageone.co.za' && $check_replyTo eq 'hitecsecurity.co.za' ) {
 			return 1
-	} 
-	if ( $check_from eq 'post.xero.com' && $check_replyTo eq 'metroroofingcontractors.co.za' ) {
+	}	elsif ( $check_from eq 'post.xero.com' && $check_replyTo eq 'metroroofingcontractors.co.za' ) {
 			return 1
-	} 
-	if ( $check_from eq 'surveys.mutualandfederal-surveys.eyerys.co.za' && $check_replyTo eq 'metroroofingcontractors.co.za' ) {
+	} elsif ( $check_from eq 'surveys.mutualandfederal-surveys.eyerys.co.za' && $check_replyTo eq 'metroroofingcontractors.co.za' ) {
 			return 1
-	}  
-	if ( $check_from eq 'enews.swanhellenic.com' && $check_replyTo eq 'pageandmoytravelgroup.com' ) {
+	} elsif ( $check_from eq 'enews.swanhellenic.com' && $check_replyTo eq 'pageandmoytravelgroup.com' ) {
 			return 1
-	}       
-	if ( $check_from eq 'fnbstatements.co.za' && $check_replyTo eq 'fnb.co.za' ) {
+	} elsif ( $check_from eq 'fnbstatements.co.za' && $check_replyTo eq 'fnb.co.za' ) {
 			return 1
-	}
-	if ( $check_from eq 'msccruises.co.za' && $check_replyTo eq 'msccruises.com' ) {
+	} elsif ( $check_from eq 'msccruises.co.za' && $check_replyTo eq 'msccruises.com' ) {
 			return 1
-	}    
-	if ( $check_from eq 'notifications.pinterest.com' && $check_replyTo eq 'reply.pinterest.com' ) {
+	} elsif ( $check_from eq 'notifications.pinterest.com' && $check_replyTo eq 'reply.pinterest.com' ) {
 			return 1
-	}    
-	if ( $check_from eq 'message.investec.com' && $check_replyTo eq 'investec.co.za' ) {
+	} elsif ( $check_from eq 'message.investec.com' && $check_replyTo eq 'investec.co.za' ) {
 			return 1
-	}     
-	if ( $check_from eq 'fnbstatements.co.za' && $check_replyTo eq 'edd.co.za' ) {
+	} elsif ( $check_from eq 'fnbstatements.co.za' && $check_replyTo eq 'edd.co.za' ) {
 			return 1
-	}    
-	if ( $check_from eq 'coct.slims.gov.za' && $check_replyTo eq 'capetown.gov.za' ) {
+	} elsif ( $check_from eq 'coct.slims.gov.za' && $check_replyTo eq 'capetown.gov.za' ) {
 			return 1
-	}     
-	if ( $check_from eq 'mrpmoney.mrp.com' && $check_replyTo eq 'mrpricegroup.com' ) {
+	} elsif ( $check_from eq 'mrpmoney.mrp.com' && $check_replyTo eq 'mrpricegroup.com' ) {
 			return 1
-	}        
-  if ( $check_from eq 'emc.co.za' && ($check_replyTo eq 'edcon.co.za' || $check_replyTo eq 'vodacom.co.za' || $check_replyTo eq 'total.co.za' || $check_replyTo eq 'wfs.co.za' || $check_replyTo eq 'tvlic.co.za' ) ) {
+	} elsif ( $check_from eq 'emc.co.za' && ($check_replyTo eq 'edcon.co.za' || $check_replyTo eq 'vodacom.co.za' || $check_replyTo eq 'total.co.za' || $check_replyTo eq 'wfs.co.za' || $check_replyTo eq 'tvlic.co.za' ) ) {
   		return 1
-	} 
-	if ( $check_replyTo eq 'mrpg.com' && ($check_from eq 'services.sheetstreet.com' || $check_from eq 'services.miladys.com' ) ) {
+	} elsif ( $check_replyTo eq 'mrpg.com' && ($check_from eq 'services.sheetstreet.com' || $check_from eq 'services.miladys.com' ) ) {
 			return 1
 	}
 	return 0;
